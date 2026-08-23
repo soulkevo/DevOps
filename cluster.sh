152375e2e6153d0aa423c0efbb6c1fec6d7c132e
Bachinskiy.V <167795546+soulkevo@users.noreply.github.com>
2026-08-24 02:21:33 +0300

    Delete cluster.sh

--- cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# cluster.sh — оновлена версія
-minikube start -p shop-main --nodes 3 --driver=docker --cpus 2 --memory 2048
-minikube addons enable csi-hostpath-driver -p shop-main
-minikube addons enable volumesnapshots -p shop-main
-minikube addons enable ingress -p shop-main
